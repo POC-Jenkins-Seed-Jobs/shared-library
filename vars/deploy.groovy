5672ff43e429c1255d90890b381d31b17a0f6a4d
rahulgandhi28 <72257523+rahulgandhi28@users.noreply.github.com>
2023-04-19 03:34:41 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -18,7 +18,7 @@ def deployToKubernetes(name, appName){
                             }
                             stages{
                                 stage("Delete old deployment"){     
-                             // this may cause an error because at the first place there are no deployments to be deleted
+                             
                                     steps{
                                         script{
                                             deploy.deleteDeployment()
@@ -43,7 +43,7 @@ def deployToKubernetes(name, appName){
 
 
 
-// Test Functions 
+// Some Test Functions 
 
 def updatek8Cluster() {
    withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'nilanjan-aws', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
@@ -66,4 +66,42 @@ def destroyk8(appname)
                 sh'''
                 kubectl apply -f Deployment.yml
                 '''
-             }        
+             }
+            
+def testDeploy(jobName, appName){
+    jobDsl scriptText: '''
+        pipelineJob('''+"\"${jobName}\""+''') {
+            definition{
+                cps{
+                    script(\'\'\'
+                        @Library(\'shared-library-nilanjan\') _
+                        pipeline{
+                            agent{
+                                label "ec2-slave"
+                            }
+                            stages{
+                                stage("Get Sources"){
+                                    steps{
+                                        script{
+                                            build.gitCheckout("https://github.com/Team-Denver/POC.Service.'''+"${appName}"+'''.git")
+                                        }
+                                    }
+                                }
+                                
+                                stage("Apply deployments"){
+                                      steps{
+                                         script{
+                                     deploy.applykubectl()
+                                 }
+                              }
+                             }
+                                
+                            }
+                            
+                        }
+                    \'\'\'.stripIndent())
+                    sandbox()
+                }
+            }
+        }'''.stripIndent()
+}            
